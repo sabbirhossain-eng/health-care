f58154b36a7ad4b9b74998175229fac0f51d6e44
Sabbir Hossain <sabbirh8181@gmail.com>
2024-07-03 14:41:28 +0600

    navbar create

--- src/Components/Shared/Nabvar/Nav.jsx
+++ src/Components/Shared/Nabvar/Nav.jsx
@@ -7,19 +7,22 @@ import {
 import { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import logo from "../../../assets/Images/logo dark.png";
+import "./Nav.css";
+import { GoArrowUpRight } from "react-icons/go";
+
 const Nav = () => {
   const { pathname } = useLocation();
   const [openNav, setOpenNav] = useState(false);
-  
+
   const navList = (
     <ul className="mt-2 mb-4 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
       <Typography className="nav-link relative">
         <Link
           className={` ${
             pathname === "/"
-              ? "text-primary-light font-bold"
+              ? "text-primary"
               : "text-gray-700 dark:text-gray-200"
-          } no-underline font-semibold text-lg  lg:hover:bg-none text-gray-800 dark:hover:text-white p-4 lg:p-0 inline-block rounded-[10px] w-full`}
+          } no-underline text-md  lg:hover:bg-none text-gray-800 dark:hover:text-white p-4 lg:p-0 inline-block rounded-[10px] w-full`}
           to={"/"}
         >
           Home
@@ -29,9 +32,9 @@ const Nav = () => {
         <Link
           className={` ${
             pathname === "/services"
-              ? "text-primary-light font-bold"
+              ? "text-primary font-bold"
               : "text-gray-700 dark:text-gray-200"
-          } no-underline font-semibold text-lg text-gray-800 dark:hover:text-white p-4 lg:p-0 inline-block rounded-[10px] w-full`}
+          } no-underline  text-md text-gray-800 dark:hover:text-white p-4 lg:p-0 inline-block rounded-[10px] w-full`}
           to={"/services"}
         >
           Services
@@ -41,9 +44,9 @@ const Nav = () => {
         <Link
           className={` ${
             pathname === "/blog"
-              ? "text-primary-light font-bold"
+              ? "text-primary font-bold"
               : "text-gray-700 dark:text-gray-200"
-          } no-underline font-semibold text-lg  lg:hover:bg-none text-gray-800 dark:hover:text-white p-4 lg:p-0 inline-block rounded-[10px] w-full`}
+          } no-underline  text-md  lg:hover:bg-none text-gray-800 dark:hover:text-white p-4 lg:p-0 inline-block rounded-[10px] w-full`}
           to={"/blog"}
         >
           Blog
@@ -53,9 +56,9 @@ const Nav = () => {
         <Link
           className={` ${
             pathname === "/contactUs"
-              ? "text-primary-light font-bold"
+              ? "text-primary font-bold"
               : "text-gray-700 dark:text-gray-200"
-          } no-underline font-semibold text-lg  lg:hover:bg-none text-gray-800  dark:hover:text-white p-4 lg:p-0 inline-block rounded-[10px] w-full`}
+          } no-underline  text-md  lg:hover:bg-none text-gray-800  dark:hover:text-white p-4 lg:p-0 inline-block rounded-[10px] w-full`}
         >
           About Us
         </Link>
@@ -78,20 +81,27 @@ const Nav = () => {
 
   return (
     <div>
-      <Navbar className="fixed z-10 max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4 bg-white dark:bg-primary-dark mx-auto">
+      <Navbar className="rounded-none px-4 py-2 lg:px-8 lg:py-4 mx-auto bg-transparent shadow-none border-none mt-2">
         <div className="flex items-center justify-between text-nowrap">
+          {/* logo */}
+          <div>
           <Typography
             onClick={handleClick}
             as="a"
             href="/"
             className="mr-4 cursor-pointer py-1.5 flex-1"
           >
-            <img src={logo} alt="logo" />
+            <img src={logo} alt="logo" className="w-[70%]" />
           </Typography>
-          <div className="flex items-center gap-4 flex-1">
+          </div>
+            {/* nav list */}
             <div className="mr-4 hidden lg:block">{navList}</div>
-            <div className="flex items-center gap-x-1 text-gray-800 dark:text-gray-200">
-              {/* navEnd */}
+            {/* Nav end */}
+            <div className="hidden lg:block items-center gap-x-1 text-gray-800">
+            <Typography className="flex items-end text-lg font-semibold border-2 border-primary rounded-xl mx-auto px-2 py-1">
+              Appointment
+              <GoArrowUpRight className="text-2xl font-extrabold"/>
+            </Typography>
             </div>
             <IconButton
               className="ml-auto h-6 w-6 text-inherit hover:bg-transparent bg-transparent focus:bg-transparent active:bg-transparent lg:hidden text-gray-800 dark:text-gray-200"
@@ -129,11 +139,15 @@ const Nav = () => {
                 </svg>
               )}
             </IconButton>
-          </div>
         </div>
         <MobileNav open={openNav}>
           {navList}
-          <div className="flex justify-around h-auto text-gray-800 dark:text-gray-200"></div>
+          <div className="no-underline  text-md text-gray-800 dark:hover:text-white p-4 lg:p-0 inline-block rounded-[10px]">
+          <Typography className="flex items-end text-lg font-semibold border-2 border-primary rounded-xl mx-auto px-2 py-1">
+              Appointment
+              <GoArrowUpRight className="text-2xl font-extrabold"/>
+            </Typography>
+          </div>
         </MobileNav>
       </Navbar>
     </div>
