c0a4a2b9fe67b7d63806cb998f12ab93541de66f
Sabbir Hossain <sabbirh8181@gmail.com>
2024-07-03 15:01:00 +0600

    container components create

---
+++ src/Components/Shared/Container/Container.jsx
@@ -0,0 +1,10 @@
+
+const Container = ({children}) => {
+    return (
+        <div className="px-40 mx-auto">
+            {children}
+        </div>
+    );
+};
+
+export default Container;
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,10 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Nav from "../Components/Shared/Nabvar/Nav";
+import Container from "../Components/Shared/Container/Container";
 
 const Main = () => {
     return (
         <div>
+            <Container>
             <Nav/>
+            </Container>
             <Outlet/>
         </div>
     );
